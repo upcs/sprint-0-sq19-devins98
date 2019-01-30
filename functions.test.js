18652003fc7a54ae382d91a6a3ed61ed58ecff7e
devins98 <smithdev20@up.edu>
2019-01-30 02:00:38 -0800

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,16 @@
+//Devin Smith
 //External Citation:
 //help writing test for js 
 //https://jestjs.io/docs/en/getting-started.html
+
+//should pass but not working
 const addTwoNumbers = require('./functions.js');
 
 test('adds given numbers 2 + 4 to equal 6', () => {
-    expect(addTwoNumbers()).toBe(4);
+    expect(addTwoNumbers()).toBe(6);
+});
+
+//should fail
+test('adds two given numbers with incorrect sum', ()=> {
+    expect(addTwoNumbers()).toBe(19);
 });
